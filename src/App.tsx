990b2e18876157f713b2186a2fc347eed5f14c81
Kartik Gupta <guptakartik961@gmail.com>
2025-07-03 17:43:51 +0530

    Initial project setup with React, TypeScript, Tailwind, react-table, and linting configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-4xl text-black font-bold bg-gray-500 text-center mt-10">
+      ✅ Tailwind CSS + CRA + TypeScript is Working!
+      <p className='bg-violet-500 rounded-lg mt-10'>Lorem ipsum dolor sit amet, consectetur architecto maiores qui perferendis modi recusandae minus!</p>
     </div>
   );
 }
